f8f16184d18d82d239aba3b0b2a48e50ec4fca96
“ardiannnnnnn” <ardianindonesia45@gmail.com>
2025-10-08 09:47:51 +0700

    ModifikasiPercobaan1

--- cetakKRS15.java
+++ cetakKRS15.java
@@ -11,6 +11,8 @@ public class cetakKRS15{
         if(uktLunas) {
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("SIlahkan cetak KRS dan minta tanda tangan DPA");
+        } else {
+            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu.");
         }
     }
 }
