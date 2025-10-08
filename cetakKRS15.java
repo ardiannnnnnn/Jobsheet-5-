88985fb0131010151ad0dc726f4cd26f93d7912a
“ardiannnnnnn” <ardianindonesia45@gmail.com>
2025-10-08 09:54:46 +0700

    Percobaan1Ternary

--- cetakKRS15.java
+++ cetakKRS15.java
@@ -8,11 +8,17 @@ public class cetakKRS15{
         System.out.println("Apakah UKT sudah lunas (true/false)");
         boolean uktLunas = sc.nextBoolean();
 
-        if(uktLunas) {
-            System.out.println("Pembayaran UKT terverifikasi");
-            System.out.println("SIlahkan cetak KRS dan minta tanda tangan DPA");
-        } else {
-            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu.");
-        }
+        // if(uktLunas) {
+        //     System.out.println("Pembayaran UKT terverifikasi");
+        //     System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        // } else {
+        //     System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu.");
+        // }
+
+        String hasil = uktLunas
+        ? "Registrasi berhasil."
+        : "Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu";
+
+        System.out.println(hasil);
     }
 }
