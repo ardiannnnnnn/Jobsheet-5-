0bb959796becc33ee293ee6bf9c31590bdb8912b
“ardiannnnnnn” <ardianindonesia45@gmail.com>
2025-10-08 10:53:02 +0700

    Tugas3

---
+++ maxmin15.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+public class maxmin15 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        // deklarasi
+        int A, B, C;
+        int max, min;
+
+        // input
+        System.out.print("Masukkan bilangan A: ");
+        A = sc.nextInt();
+        System.out.print("Masukkan bilangan B: ");
+        B = sc.nextInt();
+        System.out.print("Masukkan bilangan C: ");
+        C = sc.nextInt();
+
+        // Menentukan bilangan maksimum
+        if (A > B) {
+            if (A > C) {
+                max = A;
+            } else {
+                max = C;
+            }
+        } else {
+            if (B > C) {
+                max = B;
+            } else {
+                max = C;
+            }
+        }
+
+        // Menentukan bilangan minimum
+        if (A < B) {
+            if (A < C) {
+                min = A;
+            } else {
+                min = C;
+            }
+        } else {
+            if (B < C) {
+                min = B;
+            } else {
+                min = C;
+            }
+        }
+
+        System.out.println("Bilangan maksimum = " + max);
+        System.out.println("Bilangan minimum = " + min);
+    }
+}
